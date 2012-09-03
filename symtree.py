278bf6edc5e5ce6884db17471925065d4217b390
kaelspencer <kael@descartes>
2012-09-03 16:32:06 -0700

    Add More Path Checks
    Add a check for nested source and destination folders. Refactor the
    checks into a method.

--- symtree.py
+++ symtree.py
@@ -9,6 +9,7 @@ class LogLevel:
     Verbose = 2
 
 log_level = LogLevel.Error
+options = []
 
 def log(message, level):
     if level <= log_level:
@@ -16,13 +17,14 @@ def log(message, level):
 
 # Ensure the options are as expected.
 def init_options():
-    global log_level
+    global log_level, options
     parser = OptionParser(usage="usage: %prog [options] SOURCE DEST")
 
     parser.add_option("-v", "--verbose", help="verbose output", action="store_true", dest="verbose", default=False)
     parser.add_option("-V", "--veryverbose", help="insanely verbose output", action="store_true", dest="veryverbose", default=False)
     parser.add_option("-c", "--create", help="create destination if it does not exist", action="store_true", dest="create", default=False)
-    parser.add_option("-f", "--followsymlinks", help="causes symtree to follow symbolic links for source and destination folders", action="store_true", dest="follow", default=False)
+    parser.add_option("-f", "--followsymlinks", help="causes symtree to follow symbolic links for source and destination folders",
+        action="store_true", dest="follow", default=False)
 
     (options, args) = parser.parse_args()
 
@@ -41,12 +43,29 @@ def init_options():
     log("Source: " + source, LogLevel.Warning)
     log("Dest  : " + dest, LogLevel.Warning)
 
+    return check_paths(source, dest)
+
+# Check the validity of the paths.
+def check_paths(source, dest):
+    global options
+
     # If the source and dest are symlink, they could be nested. Default is to fail out unless user says they want to follow symlinks.
     if os.path.islink(source) and not options.follow:
-        parser.error("SOURCE is a symbolic link (enable --followsymlinks if this is intentional)")
+        log("SOURCE is a symbolic link (enable --followsymlinks if this is intentional)", LogLevel.Error)
+        return False
 
     if os.path.islink(dest) and not options.follow:
-        parser.error("DEST is a symbolic link (enable --followsymlinks if this is intentional)")
+        log("DEST is a symbolic link (enable --followsymlinks if this is intentional)", LogLevel.Error)
+        return False
+
+    # Check to make sure one path isn't below another.
+    if source.startswith(dest):
+        log("SOURCE is nested under DEST. This is not allowed.", LogLevel.Error)
+        return False
+
+    if dest.startswith(source):
+        log("DEST is nested under SOURCE. This is not allowed.", LogLevel.Error)
+        return False
 
 def main():
     init_options()
